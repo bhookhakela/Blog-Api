2121899998d1894f4e88c8b4671dca1095e1aba7
harsh_dadwal <harshdadymon@gmail.com>
2024-06-22 17:12:57 +0530

    Registered in admin page and created a test to check the working of the posts model

--- blogapi/posts/tests.py
+++ blogapi/posts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class blogtest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        testuser1= User.objects.create_user(username='testuser1',password='test123')
+        testuser1.save()
+        # Create a blog post
+        test_post=Post.objects.create(author=testuser1,title='test post',body='test')
+        test_post.save()
+    def test_blog_content(self):
+        post=Post.objects.get(id=1)
+        author=str(post.author)
+        title=str(post.title)
+        body=str(post.body)
+        self.assertEqual(author,'testuser1')
+        self.assertEqual(title,'test post')
+        self.assertEqual(body,'test')
+
