9b4518a921eb1bf8caf8ffcfdca8ec90eb93f3bc
harsh_dadwal <harshdadymon@gmail.com>
2024-06-24 16:09:33 +0530

    Added user auth and permissions for views.py

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from. import views
 urlpatterns = [
     path("", views.PostList.as_view(), name="postlist"),
     path("<int:pk>/",views.PostDetail.as_view()),
-    ]
+    path("auth/", include('rest_framework.urls'),)
+]
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 class PostList(generics.ListCreateAPIView):
     queryset=Post.objects.all()
     serializer_class=PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset=Post.objects.all()
     serializer_class = PostSerializer
 # Create your views here.
