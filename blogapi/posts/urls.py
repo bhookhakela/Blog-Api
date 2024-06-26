28ec6dec6b45a087b0e8dd01c83307619cfed639
harsh_dadwal <harshdadymon@gmail.com>
2024-06-26 12:25:20 +0530

    Implemented the post list and post detail as a ModelViewSet and added paths using SimpleRouter

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
 from. import views
+router=SimpleRouter()
+router.register("",views.PostViewSet,basename="post")
 urlpatterns = [
-    path("", views.PostList.as_view(), name="post-list"),
-    path("<int:pk>/",views.PostDetail.as_view(),name="post-detail"),
+    path('', include(router.urls)),
     path("auth/", include('rest_framework.urls')),
     path("users/", views.UserList.as_view(), name="user-list"),
     path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Post
@@ -7,18 +7,13 @@ from .serializers import PostSerializer,UserSerializer
 from .permissions import IsAuthorOrReadOnly,IsCurrentUserOrAdmin
 from django.contrib.auth import get_user_model
 
-class PostList(generics.ListCreateAPIView): #has_object_permission not called for list views so we need to override has_permission
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset=Post.objects.all()
-    serializer_class = PostSerializer
 
 class UserList(generics.ListAPIView):
     permission_classes = (IsAdminUser,)
